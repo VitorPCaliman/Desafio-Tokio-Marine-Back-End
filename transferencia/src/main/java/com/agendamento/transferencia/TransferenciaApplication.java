f02435304faf24ec04aee4a00b5142117d5a50ad
Vitor P. Caliman <101575225+VitorPCaliman@users.noreply.github.com>
2024-09-29 14:21:21 -0300

    Commit Inicial

---
+++ transferencia/src/main/java/com/agendamento/transferencia/TransferenciaApplication.java
@@ -0,0 +1,13 @@
+package com.agendamento.transferencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferenciaApplication.class, args);
+	}
+
+}
---
+++ transferencia/src/test/java/com/agendamento/transferencia/TransferenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agendamento.transferencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
